77615a45e1a9068ebdc23f0fc9d84f3991490fe1
mxm <mxm.web.develop@gmail.com>
2024-11-25 18:37:33 +0800

    解决包引入的问题

--- web/src/app/chat/page.tsx
+++ web/src/app/chat/page.tsx
@@ -1,16 +1,36 @@
-// src/app/chat/page.tsx
-import DifyChat from '@/components/DifyChat';
-import React, { Suspense } from 'react';
+'use client'
 
+// import { useEffect } from "react";
+
+import dynamic from 'next/dynamic'
+import '@mxmweb/difychat/style.css'
+// url="/myproxy"
+// token="app-jRRwbSXPpFdDUahm7QmPdyFq"
+// //token='app-H9ftP5wGNWhlqcGfy1CgyDcf'
+// mock={false}
+const DynamicHeader = dynamic(() =>
+  import('@mxmweb/difychat').then((mod) => {
+    const DynamicComponent = ({ url, token }: { url: string, token: string }) => (
+      <mod.ChatClientProvider>
+        <mod.MxMChat url={url} token={token} mock={false} />
+      </mod.ChatClientProvider>
+    );
+    return DynamicComponent; // 返回一个组件，而不是直接返回 JSX
+  }), {
+  loading: () => <p>Loading chat...</p>,
+  ssr: false
+}
+);
 const ChatPage: React.FC = () => {
+  //   useEffect(() => {
+  //     import('@mxmweb/testimport').then((mod) => {
+
+  //       const a = mod.PureFunction(4, 154)
+  //       console.log(a)
+  //     })
+  //   }, [])
   return (
-    <Suspense fallback={<div>loading lalalal</div>}>
-      <DifyChat
-        url={process.env.NEXT_PUBLIC_CHAT_URL || '/myproxy'}
-        token={process.env.NEXT_PUBLIC_CHAT_TOKEN || 'app-jRRwbSXPpFdDUahm7QmPdyFq'}
-        mock={process.env.NEXT_PUBLIC_CHAT_MOCK === 'flase'}
-      />
-    </Suspense>
+    <DynamicHeader url='https://api.dify.ai/v1' token='app-jRRwbSXPpFdDUahm7QmPdyFq' />
   );
 };
 
