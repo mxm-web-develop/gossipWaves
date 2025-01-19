96e51b12efaea5babd1b17a503bab65cab553ce9
Caesar1226 <1633490504@qq.com>
2025-01-19 23:26:46 +0800

    fix

--- applications/aigraph/src/application/views/components/Tooltip.tsx
+++ applications/aigraph/src/application/views/components/Tooltip.tsx
@@ -6,13 +6,35 @@ interface TooltipProps {
 export const CustomTooltip: React.FC<TooltipProps> = ({ data }) => {
   const d = data?.data ? data.data : {};
   return (
-    <div className="w-[326px] h-[50px] rounded-[4px] flex pt-[5px] px-[8px]">
-      <div className="bg-[#DBBDA0] w-[43px] h-[43px] rounded-[4px] text-[26px] text-white leading-[40px] text-center">
+    <div style={{ width: '326px', height: '50px', borderRadius: '4px', padding: '5px 8px 0 8px' }}>
+      <div
+        style={{
+          background: '#DBBDA0',
+          width: '43px',
+          height: '43px',
+          borderRadius: '4px',
+          fontSize: '26px',
+          color: '#fff',
+          lineHeight: '40px',
+          textAlign: 'center',
+        }}
+      >
         {d.category ? d.category.substr(0, 1) : ''}
       </div>
-      <div className="h-[40px] ml-[16px]">
-        <div className="text-[#2A2A2A] font-bold text-[16px]">{d.name}</div>
-        <div className="h-[20px] px-[10px]  bg-[#E3F0FF] text-[#055AFF] text-[12px] leading-[20px] text-center rounded-[2px] mt-[4px]">
+      <div style={{ height: '40px', marginLeft: '16px' }}>
+        <div style={{ color: '#2A2A2A', fontWeight: '16px', fontSize: '16px' }}>{d.name}</div>
+        <div
+          style={{
+            height: '20px',
+            padding: '0 10px',
+            background: '#E3F0FF',
+            fontSize: '12px',
+            lineHeight: '20px',
+            textAlign: 'center',
+            borderRadius: '4px',
+            marginTop: '4px',
+          }}
+        >
           {d.category}
         </div>
       </div>
