17d3fee61d1e26672f5a1eff60c0f41247c99d9d
Caesar1226 <1633490504@qq.com>
2025-02-14 16:22:09 +0800

    [fix] fix style

--- applications/aigraph/src/application/views/components/AppController.tsx
+++ applications/aigraph/src/application/views/components/AppController.tsx
@@ -12,62 +12,45 @@ export default function AppController(props: {
 }) {
   const { handleEvent, containerRef, openSearch, setOpenSearch } = props;
   return (
-    <div
-      className="absolute w-full"
-      style={{ height: '32px', top: '20px', left: 0, right: 0, bottom: 0 }}
-    >
-      <div
-        className="flex items-center px-3.5 justify-between w-full"
-        style={{ padding: '0 15px 0 20px' }}
-      >
-        <div>
+    <>
+      <div style={{ position: 'absolute', top: '20px', left: '20px' }}>
+        <div
+          style={{ padding: '4px 16px' }}
+          className="flex items-center justify-between hover:border-[#2468F2] bg-white cursor-pointer border border-solid border-[#EEEEEE] rounded-md"
+          onClick={() => handleEvent && handleEvent(AI_GRAPH_TYPE.BACK)}
+        >
+          <ArrowLeft color="#2468F2" size={16} className="mr-[6px]" />
+          <div className="text-sm">返回</div>
+        </div>
+      </div>
+      <div style={{ position: 'absolute', top: '20px', right: '15px' }}>
+        <div className="flex items-center justify-between" style={{ columnGap: '16px' }}>
+          <div
+            style={{
+              width: '237px',
+              height: '32px',
+              border: '1px solid #eeeeee',
+              borderRadius: '4px',
+              cursor: 'pointer',
+              display: 'flex',
+              backgroundColor: '#ffffff',
+              alignItems: 'center',
+            }}
+          >
+            <LayoutCom containerRef={containerRef} />
+            <Separator.Root className="bg-[#EEEEEE]" style={{ height: '20px', width: '1px' }} />
+            <SearchMenu handleEvent={handleEvent} open={openSearch} setOpen={setOpenSearch} />
+          </div>
           <div
             style={{ padding: '4px 16px' }}
-            className="flex items-center justify-between hover:border-[#2468F2] bg-white cursor-pointer border border-solid border-[#EEEEEE] rounded-md"
-            onClick={() => handleEvent && handleEvent(AI_GRAPH_TYPE.BACK)}
+            className="flex items-center justify-between hover:border-[#2468F2] bg-white cursor-pointer border border-solid border-[#EEEEEE] rounded-[4px]"
+            onClick={() => handleEvent && handleEvent(AI_GRAPH_TYPE.EXPORT)}
           >
-            <ArrowLeft color="#2468F2" size={16} className="mr-[6px]" />
-            <div className="text-sm">返回</div>
-          </div>
-        </div>
-        <div className="tool-bar">
-          <div className="flex items-center justify-between" style={{ columnGap: '16px' }}>
-            <div
-              style={{
-                width: '237px',
-                height: '32px',
-                border: '1px solid #eeeeee',
-                borderRadius: '4px',
-                cursor: 'pointer',
-                display: 'flex',
-                backgroundColor: '#ffffff',
-                alignItems: 'center',
-              }}
-            >
-              <LayoutCom containerRef={containerRef} />
-              <Separator.Root className="bg-[#EEEEEE]" style={{ height: '20px', width: '1px' }} />
-              <SearchMenu handleEvent={handleEvent} open={openSearch} setOpen={setOpenSearch} />
-            </div>
-            {/* <div
-              id="layoutBox"
-              style={{ borderRadius: '4px' }}
-              className="flex items-center border border-solid border-[#EEEEEE] justify-between bg-white"
-            >
-              <LayoutMenu containerRef={containerRef} />
-              <Separator.Root className="bg-[#EEEEEE]" style={{ height: '20px', width: '1px' }} />
-              <SearchMenu handleEvent={handleEvent} />
-            </div> */}
-            <div
-              style={{ padding: '4px 16px' }}
-              className="flex items-center justify-between hover:border-[#2468F2] bg-white cursor-pointer border border-solid border-[#EEEEEE] rounded-[4px]"
-              onClick={() => handleEvent && handleEvent(AI_GRAPH_TYPE.EXPORT)}
-            >
-              <img src={Download} width={16} height={16} className="mr-[6px]" />
-              <div className="text-sm text-[#555555]">导出</div>
-            </div>
+            <img src={Download} width={16} height={16} className="mr-[6px]" />
+            <div className="text-sm text-[#555555]">导出</div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
